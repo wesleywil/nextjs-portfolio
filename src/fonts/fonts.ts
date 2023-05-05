051e7d8897224af927f65165e0342552b5f6ba05
wesleywil <wesleywilsonti@gmail.com>
2023-05-05 14:31:16 -0300

    added a file to deal with the fonts that will be used in the application

---
+++ src/fonts/fonts.ts
@@ -0,0 +1,16 @@
+import { Roboto,Lora } from "next/font/google";
+
+export const roboto700 = Roboto({
+    weight: "700",
+    subsets: ["latin"],
+  });
+
+export const roboto400 = Roboto({
+    weight: "400",
+    subsets: ["latin"],
+});
+
+export const lora = Lora({
+    weight: "600",
+    subsets: ["latin"],
+});
